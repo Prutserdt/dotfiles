344dc83fc406cbe112c13ccd080f2978779b103f
Prutserdt <firstname@lastname.nl>
2020-10-29 18:36:50 +0100

    General cleaning. Removal of st and surf which are not used anymore

--- .config/dmenusurf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Script to select webpages (~/.config/urls) in Firefox by dmenu.
-# It opens: ~/.config/urls
-# This required the dmenu patch: center, which gives the dmenu -c option.
-cat ~/.config/urls | dmenu -i -c -l 30 | awk '{print $1}'| xclip -selection clipboard && surf "$(xclip -o -selection clipboard)"
