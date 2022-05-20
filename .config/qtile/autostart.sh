17743f6cda05f3095d1ce110ee757ded89aee811
Prutserdt <firstname@lastname.nl>
2022-05-20 21:12:23 +0200

    Updated

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -5,9 +5,9 @@
 # M-x and type 'org-babel-tangle' in emacs and press CTR MOD R to reload qtile.
 # Happy hacking :-)
 
-setxkbmap us &
-xmodmap $HOME/.config/rdxswitch &
-xmodmap $HOME/.config/kbswitch &
+setxkbmap us &&
+xmodmap $HOME/.config/rdxswitch &&
+xmodmap $HOME/.config/kbswitch &&
 xset r rate 300 80 &
 picom -b &
 /usr/bin/emacs --daemon &
