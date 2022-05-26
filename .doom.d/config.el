76e616d8dcff64da0c913808a4128c9171f32dc8
Prutserdt <firstname@lastname.nl>
2022-05-26 08:01:17 +0200

    Updated

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,7 +6,6 @@
 ;                  Revert Doom Emacs opinionated stuff              ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
 ;; Hide the toolbar
 (tool-bar-mode -1)
 
@@ -59,47 +58,13 @@
 (global-display-line-numbers-mode)
 (setq display-line-numbers-type 'relative)
 
-;; Visual fill column, to break the lines that otherwise will keep on going
+;; Center to the middle of the screen + set fill column to break the lines
 ;; Related: visual-fill-column-mode, global-visual-line =set-fill-column 80=
-;;(setq-default Fill-column 110)
-;;(setq set-fill-column 110)
-;;(setq set-fill-column 110)
-;;(setq fill-column 110)
-;;(auto-fill-mode 1)
-;;(setq fill-column 110)
-;;(setq-default display-fill-column-indicator-column 100)
-;;(setq-default fill-column 110)
-;; FIXME: the 110 column width is not set...? Weird,
-;;        the other tries, pasted above, also did not work...
-(setq-default visual-fill-column-width 110)
+(setq-default fill-column 110)
+(global-display-fill-column-indicator-mode)
 (add-hook 'visual-line-mode-hook 'visual-fill-column-mode)
-;; Center to the middle of the screen
 (setq-default visual-fill-column-center-text t)
 
 ;; Transparency
 (set-frame-parameter (selected-frame) 'alpha '(95 90))
 (add-to-list 'default-frame-alist '(alpha 95 90))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                              Old stuff                            ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; The lines below are removed and are kept here for a while and can be deleted later.
-
-;; Show the 80 column (Fill Column Indicator)
-;; NOTE: does not work in combination with visual-fill-column-mode
-;;       and can be removed...
-(global-display-fill-column-indicator-mode)
-
-;; Highlight the matching parenthesis pair
-;; Removed, since smartparens is already in init.el and the next line is not needed.
-;; Remove this line later when it is tested and proven to be not needed.
-;;(setq show-paren-mode 1)
-
-;; There are error in the bootscreen after the first loading: no splashscreen
-;; and no icons. This can be corrected by running doom/reload, this is not
-;; recommended and gives a void-variable doom/reload error but it seems to work for now...
-;;(doom/reload)
-
-;; Quit immediately, without a confirmation
-;;(setq confirm-kill-emacs nil)
