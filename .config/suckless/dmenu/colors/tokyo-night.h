a668e77620aaa63995c36416ece9d337c4f964d2
Prutserdt <firstname@lastname.nl>
2023-01-15 14:42:09 +0100

    Updated

---
+++ .config/suckless/dmenu/colors/tokyo-night.h
@@ -0,0 +1,9 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+    [SchemeNorm] = { "#bbbbbb", "#000000" },/*?????,  background*/
+	[SchemeSel] = { "#111111", "#c3e88d" }, /* second color: highlight of dmenu borders*/
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut] = { "#111111", "#00ffff" },
+	[SchemeMid] = { "#d7d7d7", "#1c1f24" },
+};
