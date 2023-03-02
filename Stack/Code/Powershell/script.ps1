bd240eeca48b89ee7330f2ebd65c0bdce2e50af0
Prutserdt <firstname@lastname.nl>
2023-03-02 11:13:58 +0100

    Updated

--- Stack/Code/Powershell/script.ps1
+++ Stack/Code/Powershell/script.ps1
@@ -1,3 +1,8 @@
+# Show a message to the user
+Write-Host "DO NOT PANICK!"
+# Wait for 10 seconds before running next part
+Start-Sleep -Seconds 10
+# Text to speech :-)
 Add-Type -AssemblyName System.speech
 $s = New-Object System.Speech.Synthesis.SpeechSynthesizer
 $s.Speak("Yo, yo. You have been hacked. Please wave to the camera. Ha, ha. Fortunately this is just for shits and giggles. But. Remember. You could have been seriously compromized. Over and out. Peace.")
