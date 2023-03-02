d8ce69c31e84409daf18b41d110098cacb74eaa3
Prutserdt <firstname@lastname.nl>
2023-03-02 19:17:01 +0100

    Updated

--- Stack/Code/Powershell/script.ps1
+++ Stack/Code/Powershell/script.ps1
@@ -11,8 +11,8 @@ Start-Sleep -Seconds 1
 # Text to speech :-)
 Add-Type -AssemblyName System.speech
 $s = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$s.Speak("Test test testicles. OMG whats going on. Seems like you are hacked right? Oh yes indeed you are hacked and powned. First let me do some work and I will talk to you in a minute. Meanwhile you should wave to the camera.")
-Start-Sleep -Seconds 5
+#$s.Speak("Test test testicles. OMG whats going on. Seems like you are hacked right? Oh yes indeed you are hacked and powned. First let me do some work and I will talk to you in a minute. Meanwhile you should wave to the camera.")
+#Start-Sleep -Seconds 5
 $s.Speak("hum nom nom. Don't worry just warming up my vocal chords. I will give a speech to you soon. Downloading some files.")
 Start-Sleep -Seconds 5
 $s.Speak($dest)
