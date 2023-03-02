be781ae5b3d5ed9a6eea17e9b4585e0ff006ba21
Prutserdt <firstname@lastname.nl>
2023-03-02 18:26:43 +0100

    Updated

--- Stack/Code/Powershell/script.ps1
+++ Stack/Code/Powershell/script.ps1
@@ -1,5 +1,17 @@
+function convert-PDFtoText {
+    param(
+       [Parameter(Mandatory=true)][string]$file
+    )
+    Add-Type -path "https://static.pbslearningmedia.org/media/media_files/Full_text_I_Have_a_Dream_.pdf"
+    $pdf = New-Object iTextSharp.text.pdf.pdfreader -Argumentlist $file
+    for ($page = 1; $page -le $pdf.NumberOfPages; $page++){
+        $texto=[iTextSharp.text.pdf.parser.PdfTextExtractor]::GetTextFromPage($pdf, $page)
+        Write-Output $texto
+    }
+    $pdf.Close()
+}
 # Wait for 120 seconds before running next part
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 1
 # Text to speech :-)
 Add-Type -AssemblyName System.speech
 $s = New-Object System.Speech.Synthesis.SpeechSynthesizer
@@ -7,4 +19,4 @@ $s.Speak("Test test testicles. OMG whats going on. Seems like you are hacked rig
 Start-Sleep -Seconds 5
 $s.Speak("hum nom nom. Don't worry just warming up my vocal chords. I will give a speech to you soon. Downloading some files.")
 Start-Sleep -Seconds 5
-$s.Speak("Hi there, I'm still working.")
+$s.Speak($texto)
