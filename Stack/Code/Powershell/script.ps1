1b684dc198b9aa77b733a02f12a5627056b208e4
Prutserdt <firstname@lastname.nl>
2023-03-02 20:28:27 +0100

    Updated

--- Stack/Code/Powershell/script.ps1
+++ Stack/Code/Powershell/script.ps1
@@ -1,20 +1,18 @@
 # Source URL
-$url = "https://web-archive.southampton.ac.uk/cogprints.org/1927/1/NEURON_SOMA_SIZE_IN_THE.TEXT"
+$url ="https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt"
 # Destation file
-$dest = "c:\temp\einstein.txt"
+$dest = "c:\temp\gpl.txt"
 # Download the file
 Invoke-WebRequest -Uri $url -OutFile $dest
 # Read the contents of the text file into a variable
-$text = Get-Content -Path $url -Raw
-
+$text = Get-Content -Path $dest -Raw
 # Wait for 120 seconds before running next part
 Start-Sleep -Seconds 1
 # Text to speech :-)
 Add-Type -AssemblyName System.speech
 $s = New-Object System.Speech.Synthesis.SpeechSynthesizer
-#$s.Speak("Test test testicles. OMG whats going on. Seems like you are hacked right? Oh yes indeed you are hacked and powned. First let me do some work and I will talk to you in a minute. Meanwhile you should wave to the camera.")
-#Start-Sleep -Seconds 5
-$tts.Rate   = 10
-$s.Speak("hum nom nom. Don't worry just warming up my vocal chords. I will give a speech to you soon. Downloading some files.")
-Start-Sleep -Seconds 5
+$s.Speak("Test test testicles. OMG whats going on. Seems like you are hacked right? Oh yes indeed you are hacked and powned. First let me do some work and I will talk to you in a minute. Meanwhile you should wave to the camera.")
+Start-Sleep -Seconds 3
+$s.Speak("Do re mi fa so la ti do. Don't worry just warming up my vocal chords. I will give a speech to you soon. Downloading some files.")
+Start-Sleep -Seconds 1
 $s.Speak($text)
