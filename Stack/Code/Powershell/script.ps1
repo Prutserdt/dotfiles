cb8a14c65ed6691ee422a65b5558476e60142e98
Prutserdt <firstname@lastname.nl>
2023-03-02 20:52:03 +0100

    Updated

--- Stack/Code/Powershell/script.ps1
+++ Stack/Code/Powershell/script.ps1
@@ -11,13 +11,20 @@ Start-Sleep -Seconds 1
 # Text to speech :-)
 Add-Type -AssemblyName System.speech
 $s = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$s.SelectVoice("Microsoft Zira Desktop")
+$s.Speak("yo yo yo sheeple")
+$s.SelectVoice("Microsoft David Desktop")
 $s.Speak("Test test testicles. OMG whats going on. Seems like you are hacked right?")
 $s.SelectVoice("Microsoft Zira Desktop")
-$s.Speak("Oh yes indeed, hacked and powned. Wave to the camera. Come on David, do the Lords work.")
-Start-Sleep -Seconds 3
+$s.Speak("Oh yes indeed, hacked and powned. Wave to the camera.")
+Start-Sleep -Seconds 1
+$s.SelectVoice("Microsoft David Desktop")
+$s.Speak("yeah they are checking the camera!
+$s.SelectVoice("Microsoft Zira Desktop")
+$s.Speak("whahaha")
 $s.SelectVoice("Microsoft David Desktop")
 $s.Speak("Do re mi fa so la ti do. Don't worry just warming up my vocal chords. I will give a speech to you soon. Downloading some files.")
 $s.SelectVoice("Microsoft Zira Desktop")
-$s.Speak("Hah you found the GLP. Nice. Go ahead and bore some people! Yikes.")
+$s.Speak("Hey, you found the GLP. Nice. Go ahead and bore the silly sheeple!
 $s.SelectVoice("Microsoft David Desktop")
 $s.Speak($text)
