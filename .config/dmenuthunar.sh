bb658ab0f4e296cac99c05fe1a0d0d782cf3cf3c
Prutserdt <firstname@lastname.nl>
2025-12-02 09:41:22 +0100

    Updated

--- .config/dmenuthunar.sh
+++ .config/dmenuthunar.sh
@@ -5,4 +5,5 @@ notify-send -t 1000 "Running dmenu-pcmanfm.sh" &&
 chosen=$(cat $HOME/Stack/Command_line/directories | dmenu -c -bw 2 -l 40 -i -p 'PCManFM open dir: ')
 [ -z "$chosen" ] && exit
 notify-send -t 1000 "Directory opened in PCManFM: " "$chosen" &&
-pcmanfm "$chosen"
+#pcmanfm "$chosen"
+env GTK_THEME=Adwaita:dark pcmanfm "$chosen" # Forcing the dark theme
--- .config/stack_startup.sh
+++ .config/stack_startup.sh
@@ -10,15 +10,3 @@ if [ -n "$appimage" ]; then
 else
     notify-send -t 1000 "No .AppImage file containing 'stack' in its name was found in $dir"
 fi
-#!/bin/sh
-# NOTE: This file is generated from ~/.config/README.org
-#       Please only edit that file and org-babel-tangle (emacs)
-dir="$HOME/Applications" # directory
-appimage=$(find "$dir" -name "*stack*.AppImage" -print -quit)
-
-if [ -n "$appimage" ]; then
-    notify-send -t 1000 "Starting the Stack .AppImage..." && #send notification
-    "$appimage"  # Execute the file
-else
-    notify-send -t 1000 "No .AppImage file containing 'stack' in its name was found in $dir"
-fi
